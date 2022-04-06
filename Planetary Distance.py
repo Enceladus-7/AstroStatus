f154e4b8ceaca291d3333e015f112aa49db8984d
Cameron <86626965+Enceladus-7@users.noreply.github.com>
2022-04-06 14:51:21 +0100

    Add files via upload

---
+++ Planetary Distance.py
@@ -0,0 +1,16 @@
+import numpy as np
+from skyfield.api import load
+
+def distance(object1, object2):
+    objects = load('de421.bsp')
+    body1 = objects[object1]
+    body2 = objects[object2]
+    time = load.timescale().now()
+    position = body1.at(time).observe(body2)
+    distance = position.distance()
+    return(distance)
+
+print("Welcome")
+object1 = input("Enter your first Solar body:") + ' BARYCENTER'
+object2 = input("Enter your second Solar body:") + ' EBARYCENTER'
+print(distance(object1, object2))
